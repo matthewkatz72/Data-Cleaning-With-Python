89885d744fd707dabb0474b9e8e7807b11579e5e
matthew <matthewkatz72@gmail.com>
2019-11-17 20:07:22 -0500

    Added code to view first few rows of data

---
+++ Python Data Cleaning.py
@@ -0,0 +1,9 @@
+# Importing libraries
+import pandas as pd
+import numpy as np
+
+# Read csv file into a pandas dataframe
+df = pd.read_csv("property-data.csv")
+
+# Take a look at the first few rows
+print (df.head())
