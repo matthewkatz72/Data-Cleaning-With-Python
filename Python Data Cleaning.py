130ec0bd89839fe8e8b231682a9ee46410aca3a8
Matthew Katz <matthewkatz72@gmail.com>
2019-11-18 21:03:04 -0500

    Did some testing with replacing missing values using a location based replacement

--- Python Data Cleaning.py
+++ Python Data Cleaning.py
@@ -12,6 +12,7 @@ print ("\n==========\n")
 #Looking at coloumns for null values
 print (df['NUM_BEDROOMS'])
 print (df['NUM_BEDROOMS'].isnull())
+print ("\n==========\n")
 
 # Making a list of missing value types excluding NA as NA and NaN are recognized as null
 missing_values = ["n/a", "na", "--"]
@@ -20,6 +21,7 @@ df = pd.read_csv("property-data.csv", na_values = missing_values)
 #Looking at columns after adding a list of Null values
 print (df['NUM_BEDROOMS'])
 print (df['NUM_BEDROOMS'].isnull())
+print ("\n==========\n")
 
 # Detecting numbers
 cnt=0
@@ -30,12 +32,23 @@ for row in df['OWN_OCCUPIED']:
     except ValueError:
         pass
     cnt+=1
+print ("\n==========\n")
 
 # Total missing values for each feature
-print df.isnull().sum()
+print (df.isnull().sum())
 
 # Any missing values?
-print df.isnull().values.any()
+print (df.isnull().values.any())
 
 # Total number of missing values
-print df.isnull().sum().sum()
+print (df.isnull().sum().sum())
+
+print ("\n==========\n")
+
+# Location based replacements examples
+df.loc[2,'ST_NUM'] = 125
+df.loc[6,'ST_NUM'] = 200
+df.loc[1,'SQ_FT'] = 1000
+df.loc[7,'SQ_FT'] = 1025
+
+print (df)
