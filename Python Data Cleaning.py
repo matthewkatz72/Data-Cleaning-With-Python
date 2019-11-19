60be20e023f1bfc7de8429a4e3d6ef5775652e69
Matthew Katz <matthewkatz72@gmail.com>
2019-11-18 20:45:40 -0500

    Added code for looking at null values and adding possible null values to list of recognized null values

--- Python Data Cleaning.py
+++ Python Data Cleaning.py
@@ -7,4 +7,16 @@ df = pd.read_csv("property-data.csv")
 
 # Take a look at the first few rows
 print (df.head())
-print ("test")
+print ("\n==========\n")
+
+#Looking at coloumns for null values
+print (df['NUM_BEDROOMS'])
+print (df['NUM_BEDROOMS'].isnull())
+
+# Making a list of missing value types excluding NA as NA and NaN are recognized as null
+missing_values = ["n/a", "na", "--"]
+df = pd.read_csv("property-data.csv", na_values = missing_values)
+
+#Looking at columns after adding a list of Null values 
+print (df['NUM_BEDROOMS'])
+print (df['NUM_BEDROOMS'].isnull())
